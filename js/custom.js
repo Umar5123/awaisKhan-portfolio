78af29f3691405200253afcaa90fc667b5f9f1cf
Umar5123 <umar03408684954@gmail.com>
2024-11-18 17:58:11 +0500

    first commit

---
+++ js/custom.js
@@ -0,0 +1,20 @@
+// for navigation
+$(document).ready(function(){
+    $('.humberger').click(function () {
+        $(this).toggleClass('active');
+        $('.list').toggleClass('active');
+    })
+})
+// for header  
+document.addEventListener("DOMContentLoaded", function () {
+    window.addEventListener("scroll", function () {
+        if (window.pageYOffset > 50) {
+            document.querySelector("header").classList.add("active");
+        } else {
+            document.querySelector("header").classList.remove("active");
+        }
+
+    });
+
+});
+
